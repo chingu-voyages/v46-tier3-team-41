27073940393395be3d843b89dbb48af8134ca360
xheer <83892518+xheer@users.noreply.github.com>
2023-10-04 15:48:58 -0700

    created initial landing page for not logged in
    this app should contain everything that has to do with users not being logged in

--- barber/settings.py
+++ barber/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'not_logged_in',
 ]
 
 MIDDLEWARE = [
@@ -58,7 +59,7 @@ ROOT_URLCONF = 'barber.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
---
+++ not_logged_in/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class NotLoggedInConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'not_logged_in'
